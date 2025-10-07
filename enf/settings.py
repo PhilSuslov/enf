366be0c441228ae54fdfb2f565229fe5b0210953
kirillsuslov <tayge123@mail.ru>
2025-10-07 20:42:26 +0300

    Проверка на добавление Doker

--- enf/settings.py
+++ enf/settings.py
@@ -29,8 +29,20 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'autima.online', 'www.autima.online']
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://localhost',
+    'http://autima.online',
+    'http://www.autima.online',
+    ]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
 
 # Application definition
 
